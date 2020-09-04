23a0619f30a51a519197df9015a0036950750caa
atishay <atishayjain759@gmail.com>
2020-09-04 21:34:37 +0530

    Updates

---
+++ precache-manifest.1eb4f3f9fa23f177301d227f78e6972f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a20a27157fc0da8c14017aa79f94aa",
+    "url": "/React-basic/index.html"
+  },
+  {
+    "revision": "45f534f129c77002b117",
+    "url": "/React-basic/static/css/main.39fdb43f.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/React-basic/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-basic/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f534f129c77002b117",
+    "url": "/React-basic/static/js/main.20dc37cc.chunk.js"
+  },
+  {
+    "revision": "acf1afdd2e8441278423",
+    "url": "/React-basic/static/js/runtime-main.bb1d0a3f.js"
+  }
+]);
